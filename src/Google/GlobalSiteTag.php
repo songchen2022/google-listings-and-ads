86de19473e3093684563538e21f28b0c1e1f9ff1
Chen Song <songchen@goole.com>
2022-03-14 07:42:33 +0000

    test

--- src/Google/GlobalSiteTag.php
+++ src/Google/GlobalSiteTag.php
@@ -109,7 +109,7 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 			},
 			1000005
 		);
-        add_filter('wc_add_to_cart_message_html', 'custom_action_add_to_cart', 10, 2);
+        // add_filter('wc_add_to_cart_message_html', 'custom_action_add_to_cart', 10, 2);
     // }
 
 	}
@@ -380,7 +380,7 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
     /**
 	 * Display the JavaScript code to track the purchase page.
 	 */
-	public function custom_action_add_to_cart($message, $products) {
+	public function custom_action_add_to_cart() {
 		// Only display on the add to cart button page.
 
         // $product = wc_get_product( array_key_first ( $products ) );
