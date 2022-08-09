0e65500ed21a5a5d6014af4b7048d878f41299b2
Chen Song <songchen@google.com>
2022-08-09 00:18:20 -0700

    test

--- tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
+++ tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
@@ -52,25 +52,25 @@ class PolicyComplianceCheckTest extends UnitTest {
 	}
 
 
-	// public function test_website_not_accessible() {
-	// 	$this->wc->expects( $this->any() )
-	// 				   ->method( 'get_allowed_countries' )
-	// 				   ->willReturn( ["AU" => "Australia", "AT" => "Austria", "CA" => "Canada", "US" => "United States"] );
-	// 	$this->target_audience->expects( $this->any() )
-	// 				   ->method( 'get_target_countries' )
-	// 				   ->willReturn( ["FR" => "France", "US" => "United States"] );
+	public function test_website_not_accessible() {
+		$this->wc->expects( $this->any() )
+					   ->method( 'get_allowed_countries' )
+					   ->willReturn( ["AU" => "Australia", "AT" => "Austria", "CA" => "Canada", "US" => "United States"] );
+		$this->target_audience->expects( $this->any() )
+					   ->method( 'get_target_countries' )
+					   ->willReturn( ["FR" => "France", "US" => "United States"] );
 
-	// 	$this->assertEquals($this->policy_compliance_check->is_accessible(), false);
-	// }
+		$this->assertEquals($this->policy_compliance_check->is_accessible(), false);
+	}
 
 
-	// public function test_payment_gateways() {
-	// 	$this->wc->expects( $this->any() )
-	// 				   ->method( 'get_available_payment_gateways' )
-	// 				   ->willReturn( ["PayPal", "Stripe"] );
+	public function test_payment_gateways() {
+		$this->wc->expects( $this->any() )
+					   ->method( 'get_available_payment_gateways' )
+					   ->willReturn( ["PayPal", "Stripe"] );
 
-	// 	$this->assertEquals($this->policy_compliance_check->has_payment_gateways(), true);
-	// }
+		$this->assertEquals($this->policy_compliance_check->has_payment_gateways(), true);
+	}
 
 	// public function test_empty_payment_gateways() {
 	// 	$this->wc->expects( $this->any() )
